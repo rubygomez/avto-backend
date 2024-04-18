42193cc7e308e810f924e76d2c98ce68fba64c44
rubygomez <rvbii.gomez@gmail.com>
2024-04-17 18:34:45 -0700

    user get/post route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+        render :index
+    end
+
+    def create
+        user = User.create(
+            name: params[:name],
+            email: params[:email],
+            password: params[:password],
+            birthdate: params[:birthdate]
+        )
+        if user.save
+            render json: { message: "User successfully created"}, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :bad_request
+        end
+    end
+
 end
