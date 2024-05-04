2d950c28e8c5e6ea7d08c05d6ebca64adcecb04d
rubygomez <rvbii.gomez@gmail.com>
2024-05-03 22:03:44 -0700

    removed duration from schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_04_050056) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_04_050304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +21,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_04_050056) do
     t.date "book_end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "duration"
   end
 
   create_table "cars", force: :cascade do |t|
